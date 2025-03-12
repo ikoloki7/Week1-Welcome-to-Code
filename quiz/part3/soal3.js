d9acccaac60bc48480707dc204b86625c9f0664c
ikoloki7 <haeckallalhakiim@gmail.com>
2025-03-12 18:28:44 +0700

    part3

---
+++ quiz/part3/soal3.js
@@ -0,0 +1,19 @@
+// Hint: kamu akan menggunakan kondisional juga di kasus ini.
+
+// Problem
+// Buatlah sebuah perulangan 1 - 100 dengan pertambahan counter sebanyak 1
+// Di dalam perulangan, periksa setiap angka counter:
+
+// Apabila angka counter adalah angka genap, tuliskan GENAP
+// Apabila angka counter adalah angka ganjil, tuliskan GANJIL
+
+i = 0;
+while (i < 100) {
+    i++;
+    if (i % 2 === 0){
+        console.log(`${i} = Genap`);
+    }
+    else {
+        console.log(`${i} = Ganjil`);
+    }    
+}
---
+++ quiz/part3/soal5.js
@@ -0,0 +1,12 @@
+// Problem buatlah bintang seperti berikut
+let input = 5
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+for (let i = 0; i < input; i++) {
+    console.log("*".repeat(i+1));
+}
